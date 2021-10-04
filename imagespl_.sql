34121e732f4dbf93d5babf17d0ab6a13347adea8
袁小松 <74591459+song17122328@users.noreply.github.com>
2021-10-04 13:34:54 +0800

    Add files via upload
    上一个不能直接使用，这一个可以直接使用

---
+++ imagespl_.sql
@@ -0,0 +1,11 @@
+show databases;
+use imagedata;
+-- drop table table_1;-- 
+-- create table table_1
+-- (title longtext,
+-- address varchar(200) primary key,
+-- information longtext,
+-- classification int
+-- );
+select * from table_1;
+
