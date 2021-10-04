86388b2026e9a54964ab66da8cb75d30fee7479f
袁小松 <74591459+song17122328@users.noreply.github.com>
2021-10-04 13:37:39 +0800

    上一个不能用，这个能直接用

--- imagespl.sql
+++ imagespl.sql
@@ -1,11 +1,6 @@
 show databases;
 use imagedata;
--- drop table table_1;-- 
--- create table table_1
--- (title longtext,
--- address varchar(200) primary key,
--- information longtext,
--- classification int
--- );
+drop table table_1;
+create table table_1(title longtext,address varchar(200) primary key,information longtext,classification int);
 select * from table_1;
 
